3844febbbc4510ce68c790aa835818df2ea3212c
kategrom <95121842+kategrom@users.noreply.github.com>
2022-09-10 12:29:37 +0300

    Add files via upload

---
+++ practice01/practice01.js
@@ -0,0 +1,111 @@
+// Hero, Warrior, Mage
+// attack, buyPotion, heal, move, cast spell
+
+class Hero {
+  static count = 0;
+
+  #hp = 0;
+
+  constructor(name, xp, hp, baseLvl) {
+      Hero.count++;
+
+      this.name = name;
+      this.xp = xp;
+      this.#hp = hp;
+      this.lvl = baseLvl;
+      this.position = {
+          x: 0,
+          y: 0,
+      };
+  }
+
+  get hp() {
+      return this.#hp;
+  }
+
+  set hp(value){
+      if(value > 20){
+          console.log("Здоров'я не може бути більше 20")
+      } else{
+          this.#hp = value;
+      }
+  }
+
+  takeDamage(from, weapon) {
+    this.#hp -= weapon.damage;
+    console.log(`${from.name} атакує ${this.name} за допомогою ${weapon.name} і наносить ${weapon.damage} шкоди`);
+    console.log(`У ${this.name} залишилось ${this.#hp} здоров'я`);
+}
+
+  gainXP(amount) {
+      this.xp += amount;
+      console.log(`${this.name} отримав ${amount} досвіду`);
+      if (this.xp >= 1000) {
+          this.increaseLvl();
+      }
+  }
+
+  increaseLvl() {
+      this.xp = 0;
+      this.lvl++;
+      console.log(`${this.name} отримав ${this.lvl} рівень`)
+  }
+
+  move(moveX, moveY) {
+      this.position.x += moveX;
+      this.position.y += moveY;
+      console.log(`${this.name} пішов до x : ${this.position.x}, y : ${this.position.y}`)
+  }
+}
+
+class Warrior extends Hero {
+  constructor(name, xp, hp, baseLvl, damage) {
+      super(name, xp, hp, baseLvl);
+      this.damage = damage;
+  }
+
+  attack(hero) {
+      hero.takeDamage(this.damage, this);
+  }
+}
+
+class Mage extends Hero {
+  constructor(name, xp, hp, baseLvl, spell) {
+      super(name, xp, hp, baseLvl);
+      this.spell = spell;
+  }
+
+  castSpell(hero) {
+      hero.takeDamage(this.spell.damage, this);
+  }
+}
+
+class Weapon {
+  constructor(name, damage, type){
+      this.name = name;
+      this.damage = damage;
+      this.type = type;
+  }
+}
+
+class Spell {
+  constructor(name, damage, element){
+      this.name = name;
+      this.damage = damage;
+      this.element = element;
+  }
+}
+
+const weapon1 = new Weapon('Ескалібур', 5, 'Меч');
+
+const vsevolod = new Warrior('Vsevolod', 500, 20, 0, weapon1);
+vsevolod.gainXP(500);
+vsevolod.move(-5, 5);
+
+const spell1 = new Spell('Закляття', 12, 'Вогонь')
+
+const vlad = new Mage('Vlad', 700, 15, 2, spell1);
+
+
+vsevolod.attack(vlad);
+vlad.castSpell(vsevolod);
